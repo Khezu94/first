de3488628f872e92c8a79de13a3fd1ef6c7cb089
kenzo <kg.kuba94@gmail.com>
2022-03-15 20:37:06 +0600

    first commit

---
+++ Kuba1.py
@@ -0,0 +1,17 @@
+name = 'Kuba'
+surname = 'Emilev'
+age = '27'
+work = 'Bakai'
+citizen = 'Kyrgyzstan'
+place_of_birth = 'Kemin' 
+place = 'Bishkek'
+adres = 'Kyzyl-Adyr'
+Salary = '45000'
+
+
+
+
+
+
+
+
